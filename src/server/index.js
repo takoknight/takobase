a107585a7bd3ec16a89cae0c4c258a037666219c
Jason Strutz <j@jasonstrutz.com>
2015-11-12 20:54:26 -0800

    Basic server

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,16 @@
-var express = require('express');
-var app = express();
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'))
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.send('hello world');
 });
+
+app.listen(process.env.PORT || 3000);
