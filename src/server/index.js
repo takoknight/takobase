2e7e5cb2911f91682d6f8d7323ecb98a86441bd5
Jason Strutz <j@jasonstrutz.com>
2015-11-15 20:07:26 -0800

    Add some cookie and session middleware, to get passport sessions working

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
-var express      = require('express');
-var app          = express();
-var bodyParser   = require('body-parser');
-var morgan       = require('morgan');
-var responseTime = require('response-time');
-var compression  = require('compression');
+var express       = require('express');
+var app           = express();
+var cookieParser  = require('cookie-parser');
+var cookieSession = require('cookie-session')
+var bodyParser    = require('body-parser');
+var morgan        = require('morgan');
+var responseTime  = require('response-time');
+var compression   = require('compression');
 
 app.use(morgan('dev'));
 app.use(compression());
@@ -11,6 +13,10 @@ app.use(responseTime());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cookieSession());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
